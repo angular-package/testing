5665fe4dc4d17b9b5ea0a9113fef14971d53b0d3
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-11-06 02:53:59 +0100

    test(randomString()): remove test.

--- src/test/random-string.spec.ts
+++ src/test/random-string.spec.ts
@@ -15,9 +15,5 @@ testing.describe(`randomString()`, () => {
   testing
     .it(`to 10`, () => toBe.string(randomString(10)))
     .it(`between 0 and 19500`, () => toBe.stringOfLength(randomString(19500), { min: 0, max: 19500 }))
-    .it(`to be less than 200`, () => expect(randomString(200).length).toBeLessThanOrEqual(200))
-    .it(`different chars`, () => {
-      const str = randomString(30, `0123456789`);
-      expect(str.indexOf(`45`)).toEqual(-1);
-    });
+    .it(`to be less than 200`, () => expect(randomString(200).length).toBeLessThanOrEqual(200));
 });
