794442c56c70b18200fdfaef37d2218d9f0da4a1
angularpackage <contact@angular-package.dev>
2022-01-29 20:47:12 +0100

    test(randomString()): use `stringOfLengthBetween())`.

--- src/test/random-string.spec.ts
+++ src/test/random-string.spec.ts
@@ -14,6 +14,6 @@ const toBe = new TestingToBeMatchers();
 testing.describe(`randomString()`, () => {
   testing
     .it(`to 10`, () => toBe.string(randomString(10)))
-    .it(`between 0 and 19500`, () => toBe.stringOfLength(randomString(19500), { min: 0, max: 19500 }))
+    .it(`between 0 and 19500`, () => toBe.stringOfLengthBetween(randomString(19500), 0, 19500))
     .it(`to be less than 200`, () => expect(randomString(200).length).toBeLessThanOrEqual(200));
 });
