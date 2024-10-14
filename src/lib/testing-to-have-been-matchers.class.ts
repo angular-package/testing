5fdf1a8aa0742090171769307c42164b484f949f
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-14 16:52:16 +0000

    feat(TestingToHaveBeenMatchers): add matchers.

---
+++ src/lib/testing-to-have-been-matchers.class.ts
@@ -0,0 +1,20 @@
+// Class.
+import { TestingExpect } from './testing-expect.abstract';
+// Type.
+import { ExpectType } from '../type/expect-type.type';
+
+/**
+ * Testing `ToHaveBeen` matchers.
+ */
+export class TestingToHaveBeenMatchers extends TestingExpect {
+  public called<Value>(
+    value: ExpectType<Value>,
+    expectationFailOutput?: any
+  ): this {
+    this
+      .expect(value, expectationFailOutput)
+      .toHaveBeenCalled();
+    this.setNot(false);
+    return this;
+  }
+}
