17d01400da499c3101e1507084665af2e63fd0e2
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-14 16:51:52 +0000

    style(Testing): reorder static methods to top.

--- src/lib/testing.class.ts
+++ src/lib/testing.class.ts
@@ -11,28 +11,12 @@ import {} from 'jasmine';
  */
 export class Testing extends TestingTests {
   /**
-   * Simple `class` to support testing.
-   * Creates an instance with setting for global allow executing of the `describe()` and `it()` methods,
-   * and optionally sets the list of allowed executable tests (those that execute even on the disallowed state).
-   * @param allowDescribe Allow executing `describe()` methods.
-   * @param allowIt Allow executing `it()` methods.
-   * @param executable An optional `object` of executable storage for `describe()` and `it()` methods.
-   */
-  constructor(
-    allowDescribe: boolean,
-    allowIt: boolean,
-    executable?: ExecutableTests
-  ) {
-    super(allowDescribe, allowIt, executable);
-  }
-
-  /**
    * Defines the wrapper function of the `describe()` function of jasmine with the ability to decide its execution.
    * @param description "Textual description of the group"
    * @param specDefinitions "Function for Jasmine to invoke that will define"
    * @returns The return value is a `function` that contains the `describe()` function of jasmine with the ability to decide its execution.
    */
-  static defineDescribe(
+  public static defineDescribe(
     description: string,
     specDefinitions: () => void
   ): (execute: boolean) => void {
@@ -46,11 +30,27 @@ export class Testing extends TestingTests {
    * @param timeout "Custom timeout for an async spec."
    * @returns The return value is a `function` that contains the `it()` function of jasmine with the ability to decide its execution.
    */
-  static defineIt(
+  public static defineIt(
     expectation: string,
     assertion: jasmine.ImplementationCallback,
     timeout?: number | undefined
   ): (execute: boolean) => void {
     return TestingIt.define(expectation, assertion, timeout);
+  }
+
+  /**
+   * Simple `class` to support testing.
+   * Creates an instance with setting for global allow executing of the `describe()` and `it()` methods,
+   * and optionally sets the list of allowed executable tests (those that execute even on the disallowed state).
+   * @param allowDescribe Allow executing `describe()` methods.
+   * @param allowIt Allow executing `it()` methods.
+   * @param executable An optional `object` of executable storage for `describe()` and `it()` methods.
+   */
+  constructor(
+    allowDescribe: boolean,
+    allowIt: boolean,
+    executable?: ExecutableTests
+  ) {
+    super(allowDescribe, allowIt, executable);
   }
 }
