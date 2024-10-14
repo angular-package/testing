2a7cd1dc214dd1864e2cbc842fd74c75c1536283
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-14 16:54:50 +0000

    refactor(TestingToHaveMatchers): add `been` getter with `TestingToHaveBeenMatchers` class.

--- src/lib/testing-to-have-matchers.class.ts
+++ src/lib/testing-to-have-matchers.class.ts
@@ -1,6 +1,7 @@
 // Class.
 import { TestingExpect } from './testing-expect.abstract';
 import { TestingToHaveBeenCalledMatchers } from './testing-to-have-been-called-matchers.class';
+import { TestingToHaveBeenMatchers } from './testing-to-have-been-matchers.class';
 
 // Type.
 import { ExpectType } from '../type/expect-type.type';
@@ -10,12 +11,27 @@ import { ExpectType } from '../type/expect-type.type';
  */
 export class TestingToHaveMatchers extends TestingExpect {
   /**
+   *
+   */
+  public get been(): TestingToHaveBeenMatchers {
+    return this.#been;
+  }
+
+  /**
+   * 
+   */
+  #been = new TestingToHaveBeenMatchers();
+
+  /**
    * 
    */
   public get beenCalled(): TestingToHaveBeenCalledMatchers {
     return this.#beenCalled;
   }
 
+  /**
+   * 
+   */
   #beenCalled = new TestingToHaveBeenCalledMatchers();
 
   public class<Value>(
