3181ee5306aad303f29b1d590ee7beee82562d27
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-15 09:47:52 +0000

    chore(expectations): add index.

---
+++ src/lib/expectations/index.ts
@@ -0,0 +1,7 @@
+export * from './testing-to-be-instanceof.class';
+export * from './testing-to-be.class';
+export * from './testing-to-have-been-called.class';
+export * from './testing-to-have-been.class';
+export * from './testing-to-have.class';
+export * from './testing-to-throw.class';
+export * from './testing-to.class';
