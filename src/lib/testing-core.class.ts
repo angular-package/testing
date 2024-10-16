c0a3e071bc08e92c7640b75e5627fbc96c154e40
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-16 16:13:48 +0000

    refactor(TestingCore): bring back `describe()` and `it()`, and add expectation.

--- src/lib/testing-core.class.ts
+++ src/lib/testing-core.class.ts
@@ -2,11 +2,19 @@
 import { is } from '@angular-package/type';
 // Class.
 import { TestingDescribe } from './testing-describe.class';
+import { TestingExpectation } from './testing-expectation.class';
 import { TestingIt } from './testing-it.class';
 /**
  * Core object with describe and it instances.
  */
 export abstract class TestingCore {
+  /**
+   * 
+   */
+  protected get expect(): TestingExpectation {
+    return this.#expect;
+  }
+
   /**
    * 
    */
@@ -20,6 +28,11 @@ export abstract class TestingCore {
   protected get testingIt() {
     return this.#testingIt;
   }
+
+  /**
+   * 
+   */
+  #expect = new TestingExpectation();
 
   /**
    * Privately stored instance of a `TestingDescribe`.
@@ -47,5 +60,43 @@ export abstract class TestingCore {
     }
     is.true(allowDescribe) && this.#testingDescribe.allow();
     is.true(allowIt) && this.#testingIt.allow();
+  }
+
+  /**
+   * Executes defined `describe()` function of jasmine on provided state `true` from the `execute`, and resets the counter to `0`.
+   * @param description "Textual description of the group" with an optional defined prefix indicating its unique number
+   * inside the describe.
+   * @param specDefinitions "Function for Jasmine to invoke that will define inner suites a specs"
+   * @param execute A `boolean` type value to decide whether or not execute defined `describe()` of jasmine function.
+   * @returns The return value is an instance of a child class.
+   */
+  public describe(
+    description: string,
+    specDefinitions: () => any,
+    execute?: boolean
+  ): this {
+    this.testingIt.resetCounter();
+    this.testingDescribe.describe(
+      description,
+      specDefinitions,
+      execute
+    );
+    return this;
+  }
+
+  /**
+   * Executes defined `it()` function of jasmine on provided state `true` from the `execute`.
+   * @param expectation "Textual description of what this spec is checking" with an optional its unique number when adding `[counter]`.
+   * @param assertion "Function that contains the code of your test. If not provided the test will be pending."
+   * @param execute A `boolean` type value to decide whether or not execute defined `it()` of jasmine function.
+   * @returns The return value is an instance of a child class.
+   */
+  public it(
+    expectation: string,
+    assertion: jasmine.ImplementationCallback,
+    execute?: boolean
+  ): this {
+    this.testingIt.it(expectation, assertion, execute);
+    return this;
   }
 }
