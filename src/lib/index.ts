2c8a608f699e68922082bdd799ba725561159c88
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-22 15:53:53 +0000

    chore(index): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,9 @@
+export * from './expectations';
+export * from './it';
 export { Testing } from './testing.class';
-export { TestingToBeMatchers } from './testing-tobe-matchers.class';
+export { TestingActual  } from './testing-actual.class';
+export { TestingCore } from './testing-core.class';
+export { TestingDescribe } from './testing-describe.class';
+export { TestingExpect } from './testing-expect.abstract';
+export { TestingExpectation } from './testing-expectation.class';
+export { TestingIt } from './testing-it.class';
