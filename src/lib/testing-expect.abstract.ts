8e7fa1017f7e36854c96a80c04dd37916dfcf6e3
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-22 00:43:27 +0000

    refactor(TestingExpect): reorder alphabetically methods, change access to public from protected, add `setAlready()` and `getAlready()`, add `e` to `expect()` and `expectSync()` methods for jasmine `expect()`.

--- src/lib/testing-expect.abstract.ts
+++ src/lib/testing-expect.abstract.ts
@@ -25,24 +25,6 @@ export abstract class TestingExpect {
   }
 
   /**
-   * Sets a state of a `boolean` type to invert the matcher .
-   * @param not Value of a `boolean` type indicating a state to invert the matcher.
-   * @returns The return value is an instance of a child class.
-   */
-  protected setNot(not: boolean): this {
-    is.boolean(not) && (this.#not = not);
-    return this;
-  }
-
-  /**
-   * Returns the state of invert the matcher.
-   * @returns The return value is a state of invert the matcher.
-   */
-  protected getNot(): boolean {
-    return this.#not;
-  }
-
-  /**
    * 
    */
   #already = false;
@@ -55,28 +37,69 @@ export abstract class TestingExpect {
   /**
    * Wrapper method for the `expect()` function of jasmine.
    * @param actual The value of a type captured from the `actual` that is passed to the `expect()` function of jasmine.
+   * @param expectationFailOutput
    * @returns The return value is an `object` of jasmine matchers to use.
    */
-  protected expect<T>(
+  public expect<T>(
     actual: ExpectType<T>,
-    expectationFailOutput?: any
+    expectationFailOutput?: any,
+    e = expect(actual).withContext(expectationFailOutput)
   ): jasmine.Matchers<typeof actual> {
-    return is.true(this.#not)
-      ? expect(actual).withContext(expectationFailOutput).not
-      : expect(actual).withContext(expectationFailOutput);
+    return is.true(this.#not) ? e.not : e;
   }
 
-  protected expectAsync<T, U>(
+  protected expectation<T>(
+    actual: ExpectType<T>,
+    callbackfn: (matchers: jasmine.Matchers<typeof actual>) => any,
+    expectationFailOutput?: any,
+  ): this {
+    callbackfn && callbackfn(this.expect(actual, expectationFailOutput));
+    return this;
+  }
+
+  public expectAsync<T, U>(
     actual: T | PromiseLike<T>,
     expectationFailOutput?: any,
     not?: boolean,
     already?: boolean,
+    e = expectAsync(actual).withContext(expectationFailOutput)
   ): jasmine.AsyncMatchers<T, U> {
     already && this.already;
     not && this.not;
-    const e = expectAsync(actual).withContext(expectationFailOutput);
     return is.true(this.#not)
       ? is.true(this.#already) ? e.not.already : e.not
       : is.true(this.#already) ? e.already : e;
+  }
+
+  public getAlready() {
+    return this.#already;
+  }
+
+  /**
+   * Returns the state of invert the matcher.
+   * @returns The return value is a state of invert the matcher.
+   */
+  public getNot() {
+    return this.#not;
+  }
+
+  /**
+   * 
+   * @param already 
+   * @returns 
+   */
+  public setAlready(already: boolean): this {
+    is.boolean(already) && (this.#already = already);
+    return this;
+  }
+
+  /**
+   * Sets a state of a `boolean` type to invert the matcher .
+   * @param not Value of a `boolean` type indicating a state to invert the matcher.
+   * @returns The return value is an instance of a child class.
+   */
+  public setNot(not: boolean): this {
+    is.boolean(not) && (this.#not = not);
+    return this;
   }
 }
