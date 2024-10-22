289d120aa86229479a0ca9bf5cf3e7111268d82c
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-22 15:54:14 +0000

    chore(it/index): add.

---
+++ src/lib/it/index.ts
@@ -0,0 +1,7 @@
+export * from './testing-it-to-be-instanceof.class';
+export * from './testing-it-to-be.class';
+export * from './testing-it-to-have-been-called.class';
+export * from './testing-it-to-have-been.class';
+export * from './testing-it-to-have.class';
+export * from './testing-it-to-throw.class';
+export * from './testing-it-to.class';
