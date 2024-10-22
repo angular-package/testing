86688dd9f84f90aa03bf311970d8fe241f8309c7
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-22 18:22:08 +0000

    chore(expectations/index): update.

--- src/lib/expectations/index.ts
+++ src/lib/expectations/index.ts
@@ -1,7 +1,8 @@
-export * from './testing-to-be-instanceof.class';
-export * from './testing-to-be.class';
-export * from './testing-to-have-been-called.class';
-export * from './testing-to-have-been.class';
-export * from './testing-to-have.class';
-export * from './testing-to-throw.class';
-export * from './testing-to.class';
+export { TestingToBeArrayOf } from './testing-to-be-arrayof.class';
+export { TestingToBeInstanceOf } from './testing-to-be-instanceof.class';
+export { TestingToBe } from './testing-to-be.class';
+export { TestingToHaveBeenCalled } from './testing-to-have-been-called.class';
+export { TestingToHaveBeen } from './testing-to-have-been.class';
+export { TestingToHave } from './testing-to-have.class';
+export { TestingToThrow } from './testing-to-throw.class';
+export { TestingTo } from './testing-to.class';
