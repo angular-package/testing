b116b06840bc6f7cd0de5b9d73943ae56ef35669
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-23 00:50:31 +0000

    chore(lib/index): export specific names instead of */

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,23 @@
-export * from './expectations';
-export * from './it';
+export {
+  TestingTo,
+  TestingToBe,
+  TestingToBeArrayOf,
+  TestingToBeInstanceOf,
+  TestingToHave,
+  TestingToHaveBeen,
+  TestingToHaveBeenCalled,
+  TestingToThrow,
+} from './expectations';
+export {
+  TestingItTo,
+  TestingItToBe,
+  TestingItToBeArrayOf,
+  TestingItToBeInstanceOf,
+  TestingItToHave,
+  TestingItToHaveBeen,
+  TestingItToHaveBeenCalled,
+  TestingItToThrow,
+} from './it';
 export { Testing } from './testing.class';
 export { TestingActual  } from './testing-actual.class';
 export { TestingCore } from './testing-core.class';
