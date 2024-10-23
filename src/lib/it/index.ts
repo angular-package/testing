21d71eb9a8411c8846b2528ed0d7d04105bc19c5
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-23 00:49:33 +0000

    refactor(it/index): export specific name instead of *.

--- src/lib/it/index.ts
+++ src/lib/it/index.ts
@@ -1,7 +1,8 @@
-export * from './testing-it-to-be-instanceof.class';
-export * from './testing-it-to-be.class';
-export * from './testing-it-to-have-been-called.class';
-export * from './testing-it-to-have-been.class';
-export * from './testing-it-to-have.class';
-export * from './testing-it-to-throw.class';
-export * from './testing-it-to.class';
+export { TestingItToBeArrayOf } from './testing-it-to-be-arrayof.class';
+export { TestingItToBeInstanceOf } from './testing-it-to-be-instanceof.class';
+export { TestingItToBe } from './testing-it-to-be.class';
+export { TestingItToHaveBeenCalled } from './testing-it-to-have-been-called.class';
+export { TestingItToHaveBeen } from './testing-it-to-have-been.class';
+export { TestingItToHave } from './testing-it-to-have.class';
+export { TestingItToThrow } from './testing-it-to-throw.class';
+export { TestingItTo } from './testing-it-to.class';
