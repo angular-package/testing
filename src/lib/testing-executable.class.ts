b590e327f38b589e45a698fb6417c429de3ad7a9
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-26 22:19:58 +0000

    refactor(TestingExecutable): use `CounterConfig` and move.

--- src/lib/testing-executable.class.ts
+++ src/lib/testing-executable.class.ts
@@ -2,29 +2,41 @@
 import { guard, is } from '@angular-package/type';
 // Class.
 import { TestingCounter } from './testing-counter.class';
+// Type.
+import { CounterConfig } from '../type/counter-config.type';
 /**
- * Manages the storage of executable tests.
+ * Initialize executable storage.
+ * @class
+ * @classdesc Manages the `Set` storage of executable tests.
  */
-export class TestingExecutable extends TestingCounter {
+export class TestingExecutable<
+  CounterActive extends boolean = boolean,
+  CounterDescription extends boolean = boolean,
+> extends TestingCounter<
+  CounterActive,
+  CounterDescription
+> {
   /**
-   * An instance `Set` of unique numbers with allowed tests to execute.
+   * @description An instance `Set` of unique numbers with allowed tests to execute.
    */
   #executable: Set<number> = new Set();
 
   /**
-   * Manages the storage of executable tests.
-   * Initialize executable storage.
+   * @description Initialize executable storage.
    * @param executable An optional `array` of unique numbers type to initially set executable storage.
    */
-  constructor(executable?: Array<number>) {
-    super();
+  constructor(
+    executable?: Array<number>,
+    counter: CounterConfig<CounterActive, CounterDescription> = [true, false] as any
+  ) {
+    super(...(typeof counter === 'boolean' ? [counter, counter] : counter) as any);
     is.defined(executable)
       && guard.array(executable)
       && (this.#executable = new Set(executable));
   }
 
   /**
-   * Returns the result of the check if the provided `num` or actual `counter` is in the executable storage.
+   * @description Returns the result of the check if the provided `num` or actual `counter` is in the executable storage.
    * @returns The return value is a `boolean` type indicating whether or not the provided `num` or
    * actual stored number from the `counter` exists in the executable storage.
    */
