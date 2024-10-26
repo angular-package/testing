8f63068e5b4f40b25f4cc05bfdccba1643415a8b
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-26 22:16:54 +0000

    refactor(TestingCounter): add `active` param to off `counter` and `description` to automatically add `[counter]` to descriptions.

--- src/lib/testing-counter.class.ts
+++ src/lib/testing-counter.class.ts
@@ -1,23 +1,65 @@
 /**
  * Counter for testing purposes.
+ * @class
+ * @classdesc
  */
-export abstract class TestingCounter {
+export abstract class TestingCounter<
+  Active extends boolean = boolean,
+  Description extends boolean = boolean
+> {
   /**
-   * Privately stored counter, which by default is set to `0`.
+   * @description Status of activated counter.
+   */
+  public get counterActive(): Active {
+    return this.#active as Active;
+  }
+
+  /**
+   * @description Status of automatically joined `[counter]` in description.
+   */
+  public get counterDescription(): Description {
+    return this.#auto;
+  }
+
+  /**
+   * 
+   */
+  #active: Active;
+
+  /**
+   * 
+   */
+  #auto: Description;
+
+  /**
+   * @description Privately stored counter, which by default is set to `0`.
    */
   #counter = 0;
 
   /**
-   * Adds one number to the counter.
+   *
+   * @param active 
+   * @param description Whether to automatically join `[counter]` in description.
+   */
+  constructor(
+    active: Active = true as Active,
+    description: Description = false as Description
+  ) {
+    this.#active = active;
+    this.#auto = description;
+  }
+
+  /**
+   * @description Adds one number to the counter.
    * @returns The return value is an instance of a child class.
    */
   protected count(): this {
-    this.#counter++;
+    this.#active && this.#counter++;
     return this;
   }
 
   /**
-   * Resets counter to `0`.
+   * @description Resets counter to `0`.
    * @returns The return value is an instance of a child class.
    */
   public resetCounter(): this {
@@ -26,9 +68,29 @@ export abstract class TestingCounter {
   }
 
   /**
-   * Gets the actual counter.
+   * @description Gets the actual counter.
    */
   public getCounter(): number {
     return this.#counter;
+  }
+
+  /**
+   * @description
+   * @param description 
+   * @returns 
+   */
+  protected joinCounter<Description extends string>(description: Description): `[counter] ${Description}` {
+    return `[counter] ${description}` as `[counter] ${Description}`;
+  }
+
+  /**
+   * @description Replaces `[counter]` in description method.
+   * @param description A `string` type value.
+   * @returns The return value is a `string` type description.
+   */
+  protected replaceCounter(description: string): string {
+    return (this.#auto
+      ? this.joinCounter(description)
+      : description).replace('[counter]', `${this.getCounter()}`);
   }
 }
