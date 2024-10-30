28ac8980564ede0278120eacfbfa177dfdef7db5
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-30 22:25:07 +0000

    chore(type/index): add `type`.

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,8 +1,8 @@
-export { AllowItNumber } from "./allow-it-number.type";
-export { ArrayToObject } from './array-to-object.type';
-export { CounterConfig } from './counter-config.type';
-export { ExpectType } from './expect-type.type';
-export { InstanceOfConstructor } from './instance-of-constructor.type';
-export { IntersectionOfInstances } from './intersection-of-instances.type';
-export { PrototypeOf } from './prototype-of.type';
-export { UnionToIntersection } from './union-to-intersection.type';
+export type { AllowItNumber } from "./allow-it-number.type";
+export type { ArrayToObject } from './array-to-object.type';
+export type { CounterConfig } from './counter-config.type';
+export type { ExpectType } from './expect-type.type';
+export type { InstanceOfConstructor } from './instance-of-constructor.type';
+export type { IntersectionOfInstances } from './intersection-of-instances.type';
+export type { PrototypeOf } from './prototype-of.type';
+export type { UnionToIntersection } from './union-to-intersection.type';
