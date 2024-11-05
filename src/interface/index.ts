aa2a082a820b0ca9f95e718ff1ed2b8a77934217
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-05 04:06:50 +0000

    feat(TestingOptions): add interface for `options` in some methods.

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,3 +1,5 @@
 // Interface.
-export type { TestingObject } from './object.interface.js';
-export type { TestingPersonShape } from './person-shape.interface.js';
+export type { ExecutableTests } from "./executable-tests.interface";
+export type { TestingObject } from './object.interface';
+export type { TestingOptions } from "./testing-options.interface";
+export type { TestingPersonShape } from './person-shape.interface';
---
+++ src/interface/testing-options.interface.ts
@@ -0,0 +1,6 @@
+export interface TestingOptions {
+  not?: boolean,
+  expectation?: string,
+  expectationFailOutput?: any,
+  execute?: boolean,
+}
