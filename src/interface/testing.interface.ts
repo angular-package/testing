e6c327248a73ded6b1b1ae1417246f880acaeaec
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-11 22:17:01 +0000

    refactor(Testing): add all params.

--- src/interface/testing.interface.ts
+++ src/interface/testing.interface.ts
@@ -1,13 +1,29 @@
 // Class.
+import { TestingDescribe, TestingExpect, TestingIt } from '../lib';
 import { TestingProxy } from '../lib/testing.class';
 // Type.
 import { Constructor } from '@angular-package/type';
-import { InstanceTypes } from '../type';
+import { CounterConfig, InstanceTypes } from '../type';
+// Interface.
+import { ExecutableTests } from './executable-tests.interface';
 /**
  * 
  */
 export interface TestingInterface extends Omit<typeof TestingProxy, "prototype"> {
-  new <T extends Constructor<any>[]>(
+  new <
+    T extends Constructor<any>[],
+    Descriptions extends string = string,
+    Expectations extends string = string,  
+  >(
     testing: [...T],
+    allowDescribe?: boolean,
+    allowIt?: boolean,
+    executable?: ExecutableTests,
+    descriptions?: Descriptions[],
+    expectations?: Expectations[],
+    counter?: CounterConfig,
+    testingDescribe?: TestingDescribe,
+    testingIt?: TestingIt,
+    testingExpect?: TestingExpect
   ): TestingProxy<T> & InstanceTypes<T>;
 }
