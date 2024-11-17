90fa52634ae0c52b465d2789d65f07f153ddf8fa
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-17 23:48:43 +0000

    refactor(InstanceTypes): add `readonly` indicator.

--- src/type/instance-types.type.ts
+++ src/type/instance-types.type.ts
@@ -1,5 +1,5 @@
 import { Constructor } from '@angular-package/type';
 // Written by: ChatGPT.
-export type InstanceTypes<T extends Constructor<any>[]> = T extends [infer First, ...infer Rest]
+export type InstanceTypes<T extends readonly Constructor<any>[]> = T extends readonly [infer First, ...infer Rest]
   ? (First extends Constructor<any> ? InstanceType<First> : never) & InstanceTypes<Rest extends Constructor<any>[] ? Rest : []>
   : {};
