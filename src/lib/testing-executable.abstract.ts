c3a5ea556f84412f2a9de0a657311e0ed9543453
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-17 23:28:09 +0000

    refactor(TestingExecutable): keep execute state in `TestingExecute` class.

--- src/lib/testing-executable.abstract.ts
+++ src/lib/testing-executable.abstract.ts
@@ -1,28 +1,15 @@
 // @angular-package/type.
 import { is } from '@angular-package/type';
 // Class.
-import { TestingCounter } from './testing-counter.abstract';
+import { TestingExecute } from './testing-execute.abstract';
 // Type.
-import { AllowItNumber, CounterConfig } from '../type';
+import { Executable } from '../type';
 /**
  * Initialize executable storage.
  * @class
- * @classdesc Manages the `Set` storage of executable tests.
+ * @classdesc Manages the `Set` executable tests storage of numbers.
  */
-export abstract class TestingExecutable<
-  CounterActive extends boolean = boolean,
-  CounterDescription extends boolean = boolean,
-> extends TestingCounter<
-  CounterActive,
-  CounterDescription
-> {
-  /**
-   * @description
-   */
-  public get allowed() {
-    return this.#allow;
-  }
-
+export abstract class TestingExecutable extends TestingExecute {
   /**
    * @description Executables.
    */
@@ -31,45 +18,21 @@ export abstract class TestingExecutable<
   }
 
   /**
-   * @description Privately stored allow state of executing `describe)` method, which by default is set to `false`.
-   */
-  #allow = false;
-
-  /**
-   * @description An instance `Set` of unique numbers with allowed tests to execute.
+   * @description An instance `Set` of unique numbers allowed to execute.
    */
   #executable: Set<number> = new Set();
 
   /**
-   * @description Initialize executable storage.
-   * @param executable An optional `array` of unique numbers type to initially set executable storage.
+   * @description Initialize executable storage of numbers.
+   * @param execute
+   * @param executable An optional `array` of unique numbers or `number` to execute.
    */
   constructor(
-    allow?: boolean,
-    executable?: AllowItNumber,
-    counter: CounterConfig<CounterActive, CounterDescription> = [true, false] as any
+    execute?: boolean,
+    executable?: Executable,
   ) {
-    super(...(typeof counter === 'boolean' ? [counter, counter] : counter) as any);
-    this.#allow = is.boolean(allow) ? allow : this.#allow;
+    super(execute)
     is.defined(executable) && (this.#executable = new Set(!Array.isArray(executable) ? [executable] : executable));
-  }
-
-  /**
-   * @description Allows executing `describe()` methods.
-   * @returns The return value is an instance of `TestingDescribe`.
-   */
-  public allow(): this {
-    this.#allow = true;
-    return this;
-  }
-
-  /**
-   * @description Disallows executing `describe()` methods, which means only those specified in the executable storage can be executed.
-   * @returns The return value is an instance of `TestingDescribe`.
-   */
-  public disallow(): this {
-    this.#allow = false;
-    return this;
   }
 
   /**
@@ -77,7 +40,7 @@ export abstract class TestingExecutable<
    * @returns The return value is a `boolean` type indicating whether or not the provided `num` or
    * actual stored number from the `counter` exists in the executable storage.
    */
-  public isExecutable(uniqueNumber?: number): boolean {
-    return this.#executable.has(is.number(uniqueNumber) ? uniqueNumber : this.getCounter());
+  public isExecutable(uniqueNumber: number): boolean {
+    return this.#executable.has(uniqueNumber);
   }
 }
